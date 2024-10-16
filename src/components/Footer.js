fed7172cf9091ea1a62e6cceabac9d65b723432f
asmaa-dev1 <elhintasmaa@gmail.com>
2024-10-16 11:57:54 +0100

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <p class="footer-heart">
+        
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
